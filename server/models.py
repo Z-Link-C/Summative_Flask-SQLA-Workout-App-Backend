78291a1683993df749b1aa49a7ec2d0296c3a545
Z-Link-C <zachery.croft@gmail.com>
2026-05-23 13:18:19 -0400

    Add database models for Exercise, Workout, and WorkoutExercise with relationships

---
+++ server/models.py
@@ -0,0 +1,40 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+
+class Exercise(db.Model):
+    __tablename__ = 'exercise'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable=False)
+    equipment_needed = db.Column(db.Boolean, default=False)
+
+    workout_exercises = db.relationship('WorkoutExercise', back_populates='exercise')
+
+
+class Workout(db.Model):
+    __tablename__ = 'workout'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    duration_minutes = db.Column(db.Integer)
+    notes = db.Column(db.Text)
+
+    workout_exercises = db.relationship('WorkoutExercise', back_populates='workout')
+
+
+class WorkoutExercise(db.Model):
+    __tablename__ = 'workout_exercise'
+    id = db.Column(db.Integer, primary_key=True)
+    workout_id = db.Column(db.Integer, db.ForeignKey('workout.id'), nullable=False)
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercise.id'), nullable=False)
+    reps = db.Column(db.Integer)
+    sets = db.Column(db.Integer)
+    duration_seconds = db.Column(db.Integer)
+
+    workout = db.relationship('Workout', back_populates='workout_exercises')
+    exercise = db.relationship('Exercise', back_populates='workout_exercises')
